82367f48a08cffd416b0701defbef81cfa0b2ef0
rexchen <animal1004@hotmail.com>
2012-09-11 18:03:15 +0800

    update

--- mockup/ajax.php
+++ mockup/ajax.php
@@ -4,7 +4,7 @@ echo '
 {
     "post": [
         {
-            "message": "xxx 測試資料",
+            "message": "xxx 測試資料xxx 測試資料xxx 測試資料xxx 測試資料xxx 測試資料xxx 測試資料xxx 測試資料xxx 測試資料",
             "picture": "http://a6.sphotos.ak.fbcdn.net/hphotos-ak-snc7/418819_10151076389362736_1925098142_n.jpg",
             "link": "http://www.google.com/",
             "time": "2012-09-1T06:34:08+0000"
