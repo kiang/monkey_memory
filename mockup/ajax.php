b4120894d805c05fa83e674448caf17b12841ea9
andre lee <andretw@gmail.com>
2012-09-11 16:27:18 +0800

    update time format

--- mockup/ajax.php
+++ mockup/ajax.php
@@ -7,13 +7,13 @@ echo '
             "message": "xxx 測試資料",
             "picture": "http://a6.sphotos.ak.fbcdn.net/hphotos-ak-snc7/418819_10151076389362736_1925098142_n.jpg",
             "link": "http://www.google.com/",
-            "time": "2012/09/08"
+            "time": "2012-09-1T06:34:08+0000"
         },
         {
             "message": "xxx 測試資料",
             "picture": "http://a6.sphotos.ak.fbcdn.net/hphotos-ak-snc7/418819_10151076389362736_1925098142_n.jpg",
             "link": "http://www.google.com/",
-            "time": "2012/09/08"
+            "time": "2012-09-11T06:34:08+0000"
         }
     ]
 }';
